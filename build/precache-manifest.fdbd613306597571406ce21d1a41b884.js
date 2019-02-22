86865eb3ec63fb7cde9d3fde92e668dbd6d922b5
tanuha <tanuha@zeoalliance.com>
2019-02-22 19:28:04 +0200

    commit build

---
+++ build/precache-manifest.fdbd613306597571406ce21d1a41b884.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6968b76b366811dcbf8f",
+    "url": "/static/js/main.6968b76b.chunk.js"
+  },
+  {
+    "revision": "78830a14b29d69e9a967",
+    "url": "/static/js/2.78830a14.chunk.js"
+  },
+  {
+    "revision": "6968b76b366811dcbf8f",
+    "url": "/static/css/main.7b186930.chunk.css"
+  },
+  {
+    "revision": "7b97f0cca92054d2f8c37121b25ac19c",
+    "url": "/index.html"
+  }
+];
